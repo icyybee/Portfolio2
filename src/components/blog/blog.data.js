9d2661557ce684569d670aecaeac5084bf4dab16
Beatrice Egumandi <ushiksmandi@gmail.com>
2023-09-05 18:34:04 +0100

    updates

--- src/components/blog/blog.data.js
+++ src/components/blog/blog.data.js
@@ -12,7 +12,7 @@ export const blogData = [
         img: Blog7,
         name: 'Implementing UUIDs in React: Enhancing Performance and Security',
         date: 'May 17 2023',
-        views: 50,
+        views: 96,
         react: 2,
         site: 'https://icyybee.hashnode.dev/implementing-uuids-in-react-enhancing-performance-and-security'
     },
@@ -21,8 +21,8 @@ export const blogData = [
         img: Blog6,
         name: 'Customizing Tailwind CSS in Next.js: A Complete Guide',
         date: 'Mar 30 2023',
-        views: 443,
-        react: 2,
+        views: 1269,
+        react: 3,
         site: 'https://dev.to/icyybee/customizing-tailwind-css-in-nextjs-a-complete-guide-3jjp'
     },
     {
@@ -30,7 +30,7 @@ export const blogData = [
         img: Blog5,
         name: 'Sailing Smoothly with Next.js and TailwindCSS: How to Create Gorgeous Websites with Ease',
         date: 'Mar 28 2023',
-        views: 103,
+        views: 128,
         react: 1,
         site: 'https://dev.to/icyybee/sailing-smoothly-with-nextjs-and-tailwindcss-how-to-create-gorgeous-websites-with-ease-2c88'
     },
@@ -39,7 +39,7 @@ export const blogData = [
         img: Blog4,
         name: 'Streamlining Your React Development with Next.js: A Guide to Migration for Developers',
         date: 'Mar 26 2023',
-        views: 93,
+        views: 97,
         react: 1,
         site: 'https://dev.to/icyybee/streamlining-your-react-development-with-nextjs-a-guide-to-migration-for-developers-385'
     },
@@ -48,7 +48,7 @@ export const blogData = [
         img: Blog1,
         name: 'The Ultimate Guide to Converting React Apps to Progressive Web Apps',
         date: 'Mar 11 2023',
-        views: 3245,
+        views: 3628,
         react: 75,
         site: 'https://dev.to/icyybee/the-ultimate-guide-to-converting-react-apps-to-progressive-web-apps-1pnp'
     },
@@ -57,7 +57,7 @@ export const blogData = [
         img: Blog2,
         name: 'Creating an App with React, Apollo, and GraphQL: A Step-by-Step Guide to Fetching Data and Error Handling',
         date: 'Feb 18 2023',
-        views: 962,
+        views: 1041,
         react: 17,
         site: 'https://dev.to/icyybee/creating-an-app-with-react-apollo-and-graphql-a-step-by-step-guide-to-fetching-data-and-error-handling-5bgh'
     },
@@ -66,7 +66,7 @@ export const blogData = [
         img: Blog3,
         name: 'Firebase Error: Unauthorized Domain in Deployed Site',
         date: 'Jan 30 2023',
-        views: 253,
+        views: 539,
         react: 7,
         site: 'https://dev.to/icyybee/firebase-error-unauthorized-domain-in-deployed-site-50ed'
     },
